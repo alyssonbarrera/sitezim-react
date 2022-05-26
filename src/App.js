cf5463b611c36fca889418a645d2e4e0f2168dea
Alysson Barrera <alyssonbarrera@outlook.com>
2022-05-25 21:45:53 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>REACT É FODA</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esse é o meu Sitezim Tóp React
         </p>
         <a
           className="App-link"
